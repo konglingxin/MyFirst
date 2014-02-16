6c8872b630edcfc5b08aabd8ee36ca0bffc347be
konglingxin <332682238@qq.com>
2014-02-16 20:57:29 +0800

    首次提交

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        /// <summary>
+        /// return somestring
+        /// </summary>
+        /// <returns>result</returns>
+        public string getTest()
+        {
+            return "1";
+        }
+    }
+}
